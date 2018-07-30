7bdd6ce89fe012c4ade608741caac2a75a5b3167
Noobly387 <34463179+Noobly387@users.noreply.github.com>
2018-07-29 17:59:10 -0700

    Foundation Files
    Absolutely ESSENTIAL for setup

---
+++ command.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, arg) => {
+    let sender = message.author;
+    /*Comamnd Functions*/
+}
+
+module.exports.help = {
+    name: "Command Name"
+}
